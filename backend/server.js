4bccb0102b34160b85e0da2fe4278c4dfc86dfa2
Yingyao Lu <emilylu123@gmail.com>
2021-04-23 15:52:04 +0930

    Modeling data, Preparing sample data & data Seeder Script

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,6 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow.bold))
